8ea806674fe545c7d1222bbf2a8632ef54c14088
Zahrahaaf <zahrahfebriani@gmail.com>
2018-12-11 09:32:38 +0700

    Edited

--- clustering.py
+++ clustering.py
@@ -9,28 +9,27 @@ class kMeans:
     def __init__(self, dataset, k):
         self.data = dataset
         self.k = k
-        self.centroids = self.initialize_centroids(self.data, self.k)
-        self.clusters = list()
-    def initialize_centroids(data, k):
-        centroids = data.copy()
+        self.centroids = self.initialize_centroids()
+        self.clusters = []
+    def initialize_centroids(self):
+        centroids = self.data.copy()
         np.random.shuffle(centroids)
-        return centroids[:k]
+        return centroids[:self.k]
     def closest_centroids(self, data):
         dist = []
         #Cari jarak terpendek dari data ke centroid, return centroid ke berapa
         for j in self.centroids:
-            dist.add(euclideanDistance(data, j, data.shape[1]))
+            dist.append(euclideanDistance(data, j, data.shape[0]))
         return np.argmin(dist, axis=0)
     def clustering(self):
+        for j in range(self.k):
+            self.clusters.append([])
         for i in self.data:
             index = self.closest_centroids(i)
             self.clusters[index].append(i)
         return self.clusters
     def update_centroids(self):
         return np.mean(self.clusters, axis=0)
-            
-        
-        
         
 def loadDataset(filename):
 	ds = np.loadtxt(filename, delimiter='\t')
@@ -44,8 +43,19 @@ def euclideanDistance(instance1, instance2, length):
     
 def main():
     data = loadDataset('seeds.txt')
-    dataSeed=data[:,:7]
-    print('data :', data[0][3])
+    dataSeed= data[:,:7]
+    k=3
+    
+    obj = kMeans(dataSeed, k)
+    clusters = obj.clustering()  
+    newCentroids = obj.update_centroids()
+    
+    for i in range(k):
+        print("cluster %d" % i)
+        print(len(clusters[i]))
+    #print(clusters[0])
+    #print("\n\n\n")
+   # print(newCentroids)
     
 
 main()
